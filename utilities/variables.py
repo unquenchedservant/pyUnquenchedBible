66b96860e576b58e4fda2e3bb432467ea2813bd7
Jonathan Thorne <jonathanthorne13@gmail.com>
2023-12-03 13:54:10 -0500

    Cleaned up variables file as well as added new variables

--- utilities/variables.py
+++ utilities/variables.py
@@ -1,13 +1,22 @@
 def init():
-    global main_position
+    # Position variabls
+    global main_position # this holds where on the main menu you are 
     main_position = 1
-    global menu_type
-    menu_type = "main"
-    global reading_plan
-    reading_plan = "pgh"
     global reading_position
     reading_position = 1
-    global bible_version
-    bible_version="NIV"
     global settings_position
     settings_position = 1
+    global pref_bible_position
+    pref_bible_position = 1
+
+    # Menu Variables
+    global menu_type # this holds what menu you are on
+    menu_type = "main"
+
+    #General Settings (Needs a file)
+    global reading_plan # will hold professor grant horner or mcheyne at some point
+    reading_plan = "pgh"
+    global bible_version # will try to include as many as possible, will rely on requests
+    bible_version="NIV" 
+    global preferred_bible
+    preferred_bible = "bible_gateway" # two options: "BibleGateway" or "Logos"
