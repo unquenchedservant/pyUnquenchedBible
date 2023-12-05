c9f184cfdef10fdd959538d6cf352fdcbf61bc54
Jonathan Thorne <jonathanthorne13@gmail.com>
2023-12-04 19:54:35 -0500

    fixed some woopsies

--- utilities/file_helpers.py
+++ utilities/file_helpers.py
@@ -3,11 +3,10 @@ import json
 from utilities import variables as var
 
 def getRootPath():
-    if os.name("nt"):
-        rootPath = os.path.expanduser("~") + "AppData/Roaming/UnquenchedBible"
-    elif os.name("posix"): #wow, this is easy now, with mac and linux both being posix now
+    if os.name == "nt": #windows
+        rootPath = os.path.expanduser("~") + "\\AppData\\Roaming\\UnquenchedBible"
+    elif os.name == "posix": #wow, this is easy now, with mac and linux both being posix now
         rootPath = os.path.expanduser("~") + "/.unquenchedbible"
-    
     return rootPath 
 
 def getPreferencesPath():
