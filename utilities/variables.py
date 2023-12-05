80b751db5aaf68971873fc8ee01f51717c376fff
Jonathan Thorne <jonathanthorne13@gmail.com>
2023-12-04 21:21:52 -0500

    started adding variables needed for PGH

--- utilities/variables.py
+++ utilities/variables.py
@@ -31,3 +31,47 @@ def init():
     global preferred_bible
     preferred_bible = "bible_gateway" # two options: "BibleGateway" or "Logos"
     checkFiles()
+
+    #Reading Plan Settings (Needs a file)
+    global pgh_day
+    pgh_day = 0 # 0 is disabled
+    global pgh_list_1
+    pgh_list_1 = 1
+    global pgh_list_1_done
+    pgh_list_1_done = False
+    global pgh_list_2
+    pgh_list_2 = 1
+    global pgh_list_2_done
+    pgh_list_2_done = False
+    global pgh_list_3
+    pgh_list_3 = 1
+    global pgh_list_3_done
+    pgh_list_3_done = False
+    global pgh_list_4
+    pgh_list_4 = 1
+    global pgh_list_4_done
+    pgh_list_4_done = False
+    global pgh_list_5
+    pgh_list_5 = 1
+    global pgh_list_5_done
+    pgh_list_5_done = False
+    global pgh_list_6
+    pgh_list_6 = 1
+    global pgh_list_6_done
+    pgh_list_6_done = False
+    global pgh_list_7
+    pgh_list_7 = 1
+    global pgh_list_7_done
+    pgh_list_7_done = False
+    global pgh_list_8
+    pgh_list_8 = 1
+    global pgh_list_8_done
+    pgh_list_8_done = False
+    global pgh_list_9
+    pgh_list_9 = 1
+    global pgh_list_9_done
+    pgh_list_9_done = False
+    global pgh_list_10
+    pgh_list_10 = 1
+    global pgh_list_10_done
+    pgh_list_10_done = False
