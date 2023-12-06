106240f9d6d6e6efd7b800411bc4bb583ac30ca1
Jonathan Thorne <jonathanthorne13@gmail.com>
2023-12-06 18:34:45 -0500

    Added functionality for 5 psalms a day in the readings page

--- displays/readings.py
+++ displays/readings.py
@@ -24,21 +24,33 @@ def display(stdscr):
     curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
     title_str = "Unquenched Bible"
-    status_msg = "Press 'r' to Read | Press 'd' to Mark Done |  Press 'esc' to go back"
+    
     if var.reading_plan == "pgh":
         option_1 = "1. {}".format(lh.getPGHReading(listNum=1))
         option_2 = "2. {}".format(lh.getPGHReading(listNum=2))
         option_3 = "3. {}".format(lh.getPGHReading(listNum=3))
         option_4 = "4. {}".format(lh.getPGHReading(listNum=4))
         option_5 = "5. {}".format(lh.getPGHReading(listNum=5))
+        if var.psalms:
+            option_6a = "6. Psalms"
+            option_6b = " - {}".format(lh.getPsalms(1))
+            option_6c = " - {}".format(lh.getPsalms(2))
+            option_6d = " - {}".format(lh.getPsalms(3))
+            option_6e = " - {}".format(lh.getPsalms(4))
+            option_6f = " - {}".format(lh.getPsalms(5))
         option_6 = "6. {}".format(lh.getPGHReading(listNum=6))
         option_7 = "7. {}".format(lh.getPGHReading(listNum=7))
         option_8 = "8. {}".format(lh.getPGHReading(listNum=8))
         option_9 = "9. {}".format(lh.getPGHReading(listNum=9))
         option_10 = "10. {}".format(lh.getPGHReading(listNum=10))
-        option_11 = "11. Main Menu"
     while(True):
         stdscr.clear()
+        if (cursor_y == 11 and not var.psalms) or (cursor_y == 16 and var.psalms):
+            status_msg = "Press Enter to go Back"
+        elif cursor_y == 6 and var.psalms:
+            status_msg = "'tab to mark all 5 psalms done"
+        else:
+            status_msg = "'r' to Read | Tab to mark done | 'esc' to go back"
         m.title(stdscr, title_str)
         m.status_bar(stdscr, status_msg)
         m.menu_option(stdscr, option_1, 1, 1, cursor_y)
@@ -46,12 +58,25 @@ def display(stdscr):
         m.menu_option(stdscr, option_3, 3, 1, cursor_y)
         m.menu_option(stdscr, option_4, 4, 1, cursor_y)
         m.menu_option(stdscr, option_5, 5, 1, cursor_y)
-        m.menu_option(stdscr, option_6, 6, 1, cursor_y)
-        m.menu_option(stdscr, option_7, 7, 1, cursor_y)
-        m.menu_option(stdscr, option_8, 8, 1, cursor_y)
-        m.menu_option(stdscr, option_9, 9, 1, cursor_y)
-        m.menu_option(stdscr, option_10, 10, 1, cursor_y)
-        m.menu_option(stdscr, option_11, 11, 1, cursor_y)
+        if var.psalms:
+            m.menu_option(stdscr, option_6a, 6, 1, cursor_y)
+            m.menu_option(stdscr, option_6b, 7, 1, cursor_y)
+            m.menu_option(stdscr, option_6c, 8, 1, cursor_y)
+            m.menu_option(stdscr, option_6d, 9, 1, cursor_y)
+            m.menu_option(stdscr, option_6e, 10, 1, cursor_y)
+            m.menu_option(stdscr, option_6f, 11, 1, cursor_y)
+            m.menu_option(stdscr, option_7, 12, 1, cursor_y)
+            m.menu_option(stdscr, option_8, 13, 1, cursor_y)
+            m.menu_option(stdscr, option_9, 14, 1, cursor_y)
+            m.menu_option(stdscr, option_10, 15, 1, cursor_y)
+            m.menu_option(stdscr, "11. Main Menu", 16, 1, cursor_y)
+        else:
+            m.menu_option(stdscr, option_6, 6, 1, cursor_y)        
+            m.menu_option(stdscr, option_7, 7, 1, cursor_y)
+            m.menu_option(stdscr, option_8, 8, 1, cursor_y)
+            m.menu_option(stdscr, option_9, 9, 1, cursor_y)
+            m.menu_option(stdscr, option_10, 10, 1, cursor_y)
+            m.menu_option(stdscr, "11. Main Menu", 11, 1, cursor_y)
 
         stdscr.move(cursor_y, cursor_x)
         stdscr.refresh()
@@ -61,18 +86,25 @@ def display(stdscr):
             mh.back()
         elif k == curses.KEY_UP:
             cursor_y -= 1
-            if cursor_y == 0:
+            if cursor_y == 0 and var.psalms:
+                cursor_y = 16
+            elif cursor_y == 0 and not var.psalms:
                 cursor_y = 11
             var.reading_position = cursor_y
         elif k == curses.KEY_DOWN:
             cursor_y += 1
-            if cursor_y == 12:
-                cursor_y = 1
+            if var.psalms:
+                if cursor_y == 17:
+                    cursor_y = 1
+            else:
+                if cursor_y == 12:
+                    cursor_y = 1
             var.reading_position = cursor_y
-        elif k == 10 and cursor_y == 11:
-            var.reading_position = 1
-            mh.back()
-        elif k == ord('d'):
+        elif k == 10:
+            if (var.psalms and cursor_y == 16) or (not var.psalms and cursor_y == 11):
+                var.reading_position = 1
+                mh.back()
+        elif k == 9: #TODO: add logic
             if cursor_y == 1:
                 print("Cursor position 1")
             elif cursor_y == 2:
@@ -83,6 +115,7 @@ def display(stdscr):
                 print("Cursor position 4")
             elif cursor_y == 5:
                 print("cursor position 5")
+            #TODO: add logic for psalms
             elif cursor_y == 6:
                 print("Cursor position 6")
             elif cursor_y == 7:
@@ -95,31 +128,66 @@ def display(stdscr):
                 print("cursor position 10")
         elif k == ord('r'):
             parts = []
-            if cursor_y == 1:
-                parts = option_1.split()[1:]
-            elif cursor_y == 2:
-                parts = option_2.split()[1:]
-            elif cursor_y == 3:
-                parts = option_3.split()[1:]
-            elif cursor_y == 4:
-                parts = option_4.split()[1:]
-            elif cursor_y == 5:
-                parts = option_5.split()[1:]
-            elif cursor_y == 6:
-                parts = option_6.split()[1:]
-            elif cursor_y == 7:
-                parts = option_7.split()[1:]
-            elif cursor_y == 8:
-                parts = option_8.split()[1:]
-            elif cursor_y == 9:
-                parts = option_9.split()[1:]
-            elif cursor_y == 10:
-                parts = option_10.split()[1:]
-            if cursor_y != 11:
-                if var.preferred_bible == "bible_gateway":
-                    sh.openBibleGateway(parts)
-                elif var.preferred_bible == "logos":
-                    sh.openLogos(parts)
+            if not var.psalms:
+                if cursor_y == 1:
+                    parts = option_1.split()[1:]
+                elif cursor_y == 2:
+                    parts = option_2.split()[1:]
+                elif cursor_y == 3:
+                    parts = option_3.split()[1:]
+                elif cursor_y == 4:
+                    parts = option_4.split()[1:]
+                elif cursor_y == 5:
+                    parts = option_5.split()[1:]
+                elif cursor_y == 6:
+                    parts = option_6.split()[1:]
+                elif cursor_y == 7:
+                    parts = option_7.split()[1:]
+                elif cursor_y == 8:
+                    parts = option_8.split()[1:]
+                elif cursor_y == 9:
+                    parts = option_9.split()[1:]
+                elif cursor_y == 10:
+                    parts = option_10.split()[1:]
+                if cursor_y != 11:
+                    if var.preferred_bible == "bible_gateway":
+                        sh.openBibleGateway(parts)
+                    elif var.preferred_bible == "logos":
+                        sh.openLogos(parts)
+            else:
+                if cursor_y == 1:
+                    parts = option_1.split()[1:]
+                elif cursor_y == 2:
+                    parts = option_2.split()[1:]
+                elif cursor_y == 3:
+                    parts = option_3.split()[1:]
+                elif cursor_y == 4:
+                    parts = option_4.split()[1:]
+                elif cursor_y == 5:
+                    parts = option_5.split()[1:]
+                elif cursor_y == 7:
+                    parts = option_6b.split()[1:]
+                elif cursor_y == 8:
+                    parts = option_6c.split()[1:]
+                elif cursor_y == 9:
+                    parts = option_6d.split()[1:]
+                elif cursor_y == 10:
+                    parts = option_6e.split()[1:]
+                elif cursor_y == 11:
+                    parts = option_6f.split()[1:]
+                elif cursor_y == 12:
+                    parts = option_7.split()[1:]
+                elif cursor_y == 13:
+                    parts = option_8.split()[1:]
+                elif cursor_y == 14:
+                    parts = option_9.split()[1:]
+                elif cursor_y == 15:
+                    parts = option_10.split()[1:]
+                if cursor_y != 16 and cursor_y != 6:
+                    if var.preferred_bible == "bible_gateway":
+                        sh.openBibleGateway(parts)
+                    elif var.preferred_bible == "logos":
+                        sh.openLogos(parts)
         elif k == ord('1'):
             cursor_y = 1
             var.reading_position = cursor_y
