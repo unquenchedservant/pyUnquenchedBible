48d7de0bce43d77a44719a3aa11abe33d8e27eb6
Jon Thorne <jon@jonthorne.com>
2025-02-04 21:55:55 -0500

    Toggle for Psalms (5 psalms per day)

--- UI/main_screen.py
+++ UI/main_screen.py
@@ -28,6 +28,8 @@ class UnquenchedBible(QMainWindow):
         self.is_complete = False
         self.menu = None
         self.readingMenu = None
+        self.settingsMenu = None
+        self.psalms = False
         if self.settings.value('readingPlan','PGH') == 'PGH': # PGH, Mcheyne
             self.initPGH()
         else:
@@ -36,14 +38,19 @@ class UnquenchedBible(QMainWindow):
     def initPGH(self):
         self.setWindowTitle(dt.now().strftime('%m-%d'))
         main_layout = QVBoxLayout()
-        
+        self.psalms = self.settings.value('psalms', False)
+        if self.psalms == "true":
+            self.psalms = True
+        if self.psalms == "false":
+            self.psalms = False
+        print("Psalms: ", self.psalms)
         reading_card_layout = QGridLayout()
         self.card1 = card.ReadingCard(self, 'Gospels', lh.pgh_1[int(self.settings.value("pghList1", 1))-1], listNum=1, settings=self.settings, list=lh.pgh_1)
         self.card2 = card.ReadingCard(self, 'Pentateuch', lh.pgh_2[int(self.settings.value("pghList2", 1))-1], listNum=2, settings=self.settings, list=lh.pgh_2)
         self.card3 = card.ReadingCard(self, 'Epistles I', lh.pgh_3[int(self.settings.value("pghList3", 1))-1], listNum=3, settings=self.settings, list=lh.pgh_3)
         self.card4 = card.ReadingCard(self, 'Epistles II', lh.pgh_4[int(self.settings.value("pghList4", 1))-1], listNum=4, settings=self.settings, list=lh.pgh_4)
         self.card5 = card.ReadingCard(self, 'Poetry', lh.pgh_5[int(self.settings.value("pghList5", 1))-1], listNum=5, settings=self.settings, list=lh.pgh_5)
-        self.card6 = card.ReadingCard(self, 'Psalms', lh.pgh_6[int(self.settings.value("pghList6", 1))-1], listNum=6, settings=self.settings, list=lh.pgh_6)
+        self.card6 = card.ReadingCard(self, 'Psalms', lh.pgh_6[int(self.settings.value("pghList6", 1))-1], listNum=6, settings=self.settings, list=lh.pgh_6, psalms=self.psalms)
         self.card7 = card.ReadingCard(self, 'Proverbs', lh.pgh_7[int(self.settings.value("pghList7", 1))-1], listNum=7, settings=self.settings, list=lh.pgh_7)
         self.card8 = card.ReadingCard(self, 'History', lh.pgh_8[int(self.settings.value("pghList8", 1))-1], listNum=8, settings=self.settings, list=lh.pgh_8)
         self.card9 = card.ReadingCard(self, 'Prophets', lh.pgh_9[int(self.settings.value("pghList9", 1))-1], listNum=9, settings=self.settings, list=lh.pgh_9)
@@ -62,7 +69,6 @@ class UnquenchedBible(QMainWindow):
 
         self.markDoneButton = QPushButton('Mark All Lists Done')
         self.markDoneButton.clicked.connect(self.markAllDone)
-        print(self.settings.value('pghlistsDone', 0))
         if int(self.settings.value('pghlistsDone', 0)) > 0 and int(self.settings.value('pghlistsDone', 0)) < 10:
             self.markDoneButton.setText('Mark Remaining Lists Done')
         elif int(self.settings.value('pghlistsDone', 0)) == 10:
@@ -115,22 +121,44 @@ class UnquenchedBible(QMainWindow):
 
     def handleMenu(self):
         if not self.menu:
-            print("Test")
             self.menu = self.menuBar()
-            settingsMenu = self.menu.addMenu('Settings')
+            self.settingsMenu = self.menu.addMenu('Settings')
             self.readingMenu = QMenu('Reading Plan', self)   
-            settingsMenu.addMenu(self.readingMenu)
+            self.settingsMenu.addMenu(self.readingMenu)
+        for action in self.settingsMenu.actions():
+            self.settingsMenu.removeAction(action)
         for action in self.readingMenu.actions():
             self.readingMenu.removeAction(action)
+        self.settingsMenu.addMenu(self.readingMenu)
+
+        # Reading Plan Toggle
         pghChecked = "✓" if self.settings.value('readingPlan', 'PGH') == 'PGH' else ''
         mcheyneChecked = "✓" if self.settings.value('readingPlan', 'PGH') == 'Mcheyne' else ''
         pghAction = QAction('pgh{}'.format(pghChecked), self)
-        mcheyneAction = QAction('mcheyne'.format(mcheyneChecked), self)
-        
+        mcheyneAction = QAction('mcheyne{}'.format(mcheyneChecked), self)
         pghAction.triggered.connect(lambda: self.setActivePlan('PGH'))
         mcheyneAction.triggered.connect(lambda: self.setActivePlan('Mcheyne'))
         self.readingMenu.addAction(pghAction)
         self.readingMenu.addAction(mcheyneAction)
+
+        # Psalms Toggle
+        psalmsChecked = "✓" if self.settings.value('psalms', False) else ''
+        psalmsAction = QAction("Psalms{}".format(psalmsChecked), self)
+        psalmsAction.triggered.connect(lambda: self.changePsalms())
+        self.settingsMenu.addAction(psalmsAction)
+
+        self.settingsMenu.addAction('Exit', self.close)
+
+
+    def changePsalms(self):
+        if self.settings.value('psalms', False):
+            self.settings.setValue('psalms', False)
+        else:
+            self.settings.setValue('psalms', True)
+        if self.settings.value('readingPlan', 'PGH') == 'PGH':
+            self.initPGH()
+        else:
+            self.initMcheyne()
 
     def setActivePlan(self, plan):
         self.settings.setValue('readingPlan', plan)
@@ -145,48 +173,27 @@ class UnquenchedBible(QMainWindow):
         else:
             plan = "mcheyne"
             max = 4
-        print("List 1 - {}".format(self.settings.value("{}List1Done".format(plan), False)))
-        print("List 2 - {}".format(self.settings.value("{}List2Done".format(plan), False)))
-        print("List 3 - {}".format(self.settings.value("{}List3Done".format(plan), False)))
-        print("List 4 - {}".format(self.settings.value("{}List4Done".format(plan), False)))
-        if max > 4:
-            print("List 5 - {}".format(self.settings.value("{}List5Done".format(plan), False)))
-            print("List 6 - {}".format(self.settings.value("{}List6Done".format(plan), False)))
-            print("List 7 - {}".format(self.settings.value("{}List7Done".format(plan), False)))
-            print("List 8 - {}".format(self.settings.value("{}List8Done".format(plan), False)))
-            print("List 9 - {}".format(self.settings.value("{}List9Done".format(plan), False)))
-            print("List 10 - {}".format(self.settings.value("{}List10Done".format(plan), False)))
         if self.settings.value("{}List1Done".format(plan), False) == "false":
             self.card1.markSingleDone()
-            print("Marking List 1")
         if self.settings.value("{}List2Done".format(plan), False) == "false":
             self.card2.markSingleDone()
-            print("Marking List 2")
         if self.settings.value("{}List3Done".format(plan), False) == "false":
             self.card3.markSingleDone()
-            print("Marking List 3")
         if self.settings.value("{}List4Done".format(plan), False) == "false":
             self.card4.markSingleDone()
-            print("Marking List 4")
         if max > 4:
             if self.settings.value("{}List5Done".format(plan), False) == "false":
                 self.card5.markSingleDone()
-                print("Marking List 5")
             if self.settings.value("{}List6Done".format(plan), False) == "false":
                 self.card6.markSingleDone()
-                print("Marking List 6")
             if self.settings.value("{}List7Done".format(plan), False) == "false":
                 self.card7.markSingleDone()
-                print("Marking List 7")
             if self.settings.value("{}List8Done".format(plan), False) == "false":
                 self.card8.markSingleDone()
-                print("Marking List 8")
             if self.settings.value("{}List9Done".format(plan), False) == "false":
                 self.card9.markSingleDone()
-                print("Marking List 9")
             if self.settings.value("{}List10Done".format(plan), False) == "false":
                 self.card10.markSingleDone()
-                print("Marking List 10")
         self.settings.setValue("{}listsDone".format(plan), max)
         self.markDoneButton.setText('Reset All Lists')
         self.markDoneButton.clicked.disconnect(self.markAllDone)
@@ -200,36 +207,26 @@ class UnquenchedBible(QMainWindow):
             plan = "mcheyne"
             max = 4
         if self.settings.value("{}List1Done".format(plan), False) == "true":
-            print("Resetting List 1")
             self.card1.resetSingle()
         if self.settings.value("{}List2Done".format(plan), False) == "true":
             self.card2.resetSingle()
-            print("Resetting List 2")
         if self.settings.value("{}List3Done".format(plan), False) == "true":
             self.card3.resetSingle()
-            print("Resetting List 3")
         if self.settings.value("{}List4Done".format(plan), False) == "true":
             self.card4.resetSingle()
-            print("Resetting List 4")
         if max > 4:
             if self.settings.value("{}List5Done".format(plan), False) == "true":
                 self.card5.resetSingle()
-                print("Resetting List 5")
             if self.settings.value("{}List6Done".format(plan), False) == "true":
                 self.card6.resetSingle()
-                print("Resetting List 6")
             if self.settings.value("{}List7Done".format(plan), False) == "true":
                 self.card7.resetSingle()
-                print("Resetting List 7")
             if self.settings.value("{}List8Done".format(plan), False) == "true":
                 self.card8.resetSingle()
-                print("Resetting List 8")
             if self.settings.value("{}List9Done".format(plan), False) == "true":
                 self.card9.resetSingle()
-                print("Resetting List 9")
             if self.settings.value("{}List10Done".format(plan), False) == "true":
                 self.card10.resetSingle()
-                print("Resetting List 10")
         self.settings.setValue("{}listsDone".format(plan), 0)
         self.markDoneButton.setText('Mark All Lists Done')
         self.markDoneButton.clicked.disconnect(self.resetAllDone)
